cbdb306d0805174bf606b152400b09127ee8162d
rahul14239 <rn0553691@gmail.com>
2024-11-12 01:30:46 -0800

    Add files via upload

---
+++ shiva.java
@@ -0,0 +1,11 @@
+public class shiva{
+public static void main(String args[]){
+float a=22,b=12,c=10;
+System.out.println(a+b);
+System.out.println(a*b);
+System.out.println(a/b);
+System.out.println(a<=b);
+System.out.println(a+b<=b+c);
+System.out.println(c+b<=b-a);
+}
+}
